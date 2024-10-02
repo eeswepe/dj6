2b35d79dc3366c031ff6b99ee01bf0d2d4b96841
eeswepe <singgihwahyupermana@gmail.com>
2024-10-02 21:14:39 +0700

    fix: fixing bug when the year are multiples of 50

--- Pemilihan2Percobaan126.java
+++ Pemilihan2Percobaan126.java
@@ -10,7 +10,9 @@ public class Pemilihan2Percobaan126 {
         if((tahun%4)==0){
             if((tahun%100)!=0)
                 System.out.println("Tahun Kabisat");
-        }else
+            else
+                System.out.println("Bukan Tahun Kabisat");
+    }else
                 System.out.println("Bukan Tahun Kabisat");
     }
 }
